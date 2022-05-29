392f65fa47c3c6fe5e8f69c67415292ddf53f1e3
Julian Ćwirko <julian.cwirko@gmail.com>
2022-05-29 16:41:20 +0200

    added filtering

--- README.md
+++ README.md
@@ -1,17 +1,32 @@
 ### elven-tools-collection-owners-csv
 
-A simple script that will take the output file from `elven-tools collection-nft-owners` and produce a predefined CSV file. There is no way to change the logic with configuration, but it is straightforward, so anyone with JavaScript knowledge can quickly modify it. It can be helpful as a base for other transformations.
+A simple script that will take the output file from `elven-tools collection-nft-owners` and produce a predefined CSV file. There is no way to change the output structure with configuration, but it is straightforward, so anyone with JavaScript knowledge can quickly modify it. It can be helpful as a base for other transformations.
 
-Usage:
+#### Usage:
 1. Clone the repository
 2. Put your `nft-collection-owners.json` in `data` folder (there is already an example there)
 3. Run the script by `npm run generate` (in the root folder)
 
-The format of the output is:
+#### The format of the output is:
 
 - `<erd_address>,<nft_token_identifier>,<nft_token_asset_file_name>`
 
 Addresses with multiple tokens will be displayed as additional CSV rows. See the example in `data/output.csv`. The output won't cover the number of tokens per address. You would need to count the rows with the same address.
+
+#### Filtering the CSV output
+
+You can add two additional filters for tokens. In default, the elven tools CLI doesn't limit the output of the tokens array. It will limit only addresses in the output JSON. The token arrays will stay the same, even if addresses were filtered by specific file names of the token assets.
+
+Here you can filter the CSV output to show only required NFT tokens on the list. There are two flags:
+
+- `identifiers`
+- `fileNames`
+
+Usage examples with filters: 
+
+- `npm run generate identifiers=EAPES-8f3c1f-1c87,EAPES-8f3c1f-15d6`
+- `npm run generate fileNames=7329,5589`
+- `npm run generate fileNames=5589,7329 identifiers=EAPES-8f3c1f-1bf9,EAPES-8f3c1f-1c87`
 
 ---
 
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import fs from "fs";
-import { cwd, exit } from "process";
+import { cwd, exit, argv } from "process";
 
-// Input file requiredd structure (output of the elven-tools collection-nft-owners). 
+// Input file requiredd structure (output of the elven-tools collection-nft-owners).
 // Example:
 // [
 //   {
@@ -20,25 +20,77 @@ import { cwd, exit } from "process";
 //   }
 // ]
 
+// The input file whic is the output from the 'elven-tools collection-nft-owners' command
 const inputFile = `${cwd()}/nft-collection-owners.json`;
 
 try {
   fs.accessSync(inputFile, fs.constants.R_OK | fs.constants.W_OK);
-} catch(e) {
+} catch (e) {
   console.log(e.message);
-  console.log('\nPlease provide the nft-collection-owners.json file\n');
+  console.log("\nPlease provide the nft-collection-owners.json file\n");
   exit(9);
 }
+
+// Predefined filters flags
+const filtersFlags = ["identifiers", "fileNames"];
+
+const getFilters = () => {
+  if (argv.length > 2) {
+    return argv.filter(
+      (arg) =>
+        arg.includes(`${filtersFlags[0]}=`) ||
+        arg.includes(`${filtersFlags[1]}=`)
+    );
+  }
+};
+
+const getFilterKeywords = (filters) => {
+  if (Array.isArray(filters)) {
+    const obj = {};
+    for (let filter of filters) {
+      const split = filter.split("=");
+      obj[split[0]] = split[1].split(",");
+    }
+    return obj;
+  }
+};
+
+// Main filter function if filters are set
+const filterTokens = (tokensArr) => {
+  const filtersKeysObj = getFilterKeywords(getFilters());
+  if (Array.isArray(tokensArr)) {
+    return tokensArr.filter((token) => {
+      if (
+        !filtersKeysObj ||
+        Object.keys(filtersKeysObj).length === 0 ||
+        (filtersKeysObj.fileNames &&
+          filtersKeysObj.fileNames.some((item) => item === token.metadataFileName)) ||
+        (filtersKeysObj.identifiers &&
+          filtersKeysObj.identifiers.some(
+            (item) => item === token.identifier
+          ))
+      ) {
+        return true;
+      }
+      return false;
+    });
+  }
+  return false;
+};
 
 const ownersFile = fs.readFileSync(inputFile, { encoding: "utf8" });
 const ownersJSON = JSON.parse(ownersFile);
 
 if (ownersJSON) {
-  const CSV = ownersJSON.map((item) => {
-    return item.tokens.map((token) => {
-      return `${item.owner},${token.identifier},${token.metadataFileName}`;
-    }).join('\r\n');
-  }).join('\r\n');
+  const CSV = ownersJSON
+    .map((item) => {
+      return filterTokens(item.tokens)
+        .map((token) => {
+          return `${item.owner},${token.identifier},${token.metadataFileName}`;
+        })
+        .join("\r\n");
+    })
+    .join("\r\n");
   fs.writeFileSync(`${cwd()}/output.csv`, CSV);
   console.log("Saved the output to the CSV file: data/output.csv");
 } else {
